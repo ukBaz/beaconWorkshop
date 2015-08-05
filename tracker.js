dcc8cfaeadfe3088171e777cb51c3a827f6f57aa
Sonia <soniacaldwell@hotmail.com>
2015-08-05 14:27:12 +0100

    Change formatting for better print format. Still more to do.

--- tracker.js
+++ tracker.js
@@ -11,22 +11,28 @@ UriBeaconScanner.on('discover', function(uriBeacon) {
    }
 });
 
-/**
- * Main Code
- */
+// Main Code
+
+// If 2 beacon ids not given give error message and exit.
 if(process.argv.length < 4) {
-    console.log("Need to specify two beacons to track");        // If 2 beacon ids not given give error message and exit.
+    console.log("Need to specify two beacons to track");
     process.exit(1);
 }
 
-var CLOSE_RANGE = 2;                                            // When beacon distance < CLOSE_RANGE away, red light will go on.
-                                                                // When beacon distance is NEAR RANGE, between CLOSE and FAR, amber light goes on.
-var FAR_RANGE = 10;                                             // When beacon distance > FAR_RANGE away, green light will go on.
+var CLOSE_RANGE = 2; // distance < CLOSE_RANGE, red light will go on.
+                     // CLOSE_RANGE <= distance <= FAR_RANGE, amber light goes on.
+var FAR_RANGE = 10;  // distance > FAR_RANGE, green light will go on.
 
 var beacon1RegExp = new RegExp(process.argv[2]);
 var beacon2RegExp = new RegExp(process.argv[3]);
-var beacon1_id = new BeaconLights(process.argv[2], 21, 20, 16); // Function links beacon id and the pin numbers to display red/amber/green to. See appendix 1.
-var beacon2_id = new BeaconLights(process.argv[3], 7, 8, 25);   // Function links beacon id and the pin numbers to display red/amber/green to. See appendix 1.
-console.log('Tracking Beacon1: ' + process.argv[2]);            // Prints progress message to terminal.
-console.log('Tracking Beacon2: ' + process.argv[3]);            // Prints progress message to terminal.
+
+// Links beacon id and pin numbers to display red/amber/green to for each beacon.
+// See appendix 1.
+var beacon1_id = new BeaconLights(process.argv[2], 21, 20, 16);
+var beacon2_id = new BeaconLights(process.argv[3], 7, 8, 25);
+
+// Prints progress messages to terminal.
+console.log('Tracking Beacon1: ' + process.argv[2]);
+console.log('Tracking Beacon2: ' + process.argv[3]);
+
 UriBeaconScanner.startScanning(true);
